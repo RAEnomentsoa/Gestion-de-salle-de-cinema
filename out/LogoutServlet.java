4394b40e767c30540264ccad0d7aa6526ce8f048
RAEnomentsoa <arakotomalala46@gmail.com>
2026-01-09 11:35:39 +0300

    login fix
    logout

--- out/LogoutServlet.java
+++ out/LogoutServlet.java
@@ -11,7 +11,8 @@ public class LogoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         HttpSession session = req.getSession(false);
-        if (session != null) session.invalidate();
+        if (session != null)
+            session.invalidate();
         resp.sendRedirect("login");
     }
 }
