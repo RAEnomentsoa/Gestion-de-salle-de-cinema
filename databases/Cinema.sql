326c762881a011a4eb8afccc8ca2cf325ef150f7
RAEnomentsoa <arakotomalala46@gmail.com>
2026-01-13 14:34:55 +0300

    database re arange data / database
    data arange

--- databases/Cinema.sql
+++ databases/Cinema.sql
@@ -20,8 +20,7 @@ CREATE TABLE cinema (
   created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO cinema (name, address, city, status)
-VALUES ('Cine Star', 'Avenue de l''Independance', 'Antananarivo', 'ACTIVE');
+
 
 
 -- Unicité basique (optionnelle) : un cinéma de même nom dans la même ville
@@ -45,14 +44,6 @@ CREATE TABLE room (
   CONSTRAINT uq_room_cinema_name
     UNIQUE (cinema_id, name)
 );
-
-INSERT INTO room (cinema_id, name, status)
-VALUES
-(1, 'Salle 1', 'ACTIVE'),
-(1, 'Salle 2', 'ACTIVE'),
-(1, 'Salle 3', 'MAINTENANCE');
-
-
 
 CREATE INDEX idx_room_cinema_id ON room(cinema_id);
 
@@ -79,43 +70,6 @@ CREATE TABLE seat (
 CREATE INDEX idx_seat_room_id ON seat(room_id);
 CREATE INDEX idx_seat_room_active ON seat(room_id, is_active);
 
-
-INSERT INTO seat (room_id, row_label, seat_number, seat_type, is_active) VALUES
--- Salle 1
-(1, 'A', 1, 'STANDARD', TRUE),
-(1, 'A', 2, 'STANDARD', TRUE),
-(1, 'A', 3, 'STANDARD', TRUE),
-(1, 'A', 4, 'STANDARD', TRUE),
-(1, 'A', 5, 'STANDARD', TRUE),
-(1, 'A', 6, 'STANDARD', TRUE),
-(1, 'A', 7, 'STANDARD', TRUE),
-(1, 'A', 8, 'STANDARD', TRUE),
-(1, 'A', 9, 'STANDARD', TRUE),
-(1, 'A', 10, 'STANDARD', TRUE),
-
--- Salle 2
-(2, 'A', 1, 'STANDARD', TRUE),
-(2, 'A', 2, 'STANDARD', TRUE),
-(2, 'A', 3, 'STANDARD', TRUE),
-(2, 'A', 4, 'STANDARD', TRUE),
-(2, 'A', 5, 'STANDARD', TRUE),
-(2, 'A', 6, 'STANDARD', TRUE),
-(2, 'A', 7, 'STANDARD', TRUE),
-(2, 'A', 8, 'STANDARD', TRUE),
-(2, 'A', 9, 'STANDARD', TRUE),
-(2, 'A', 10, 'STANDARD', TRUE),
-
--- Salle 3
-(3, 'A', 1, 'STANDARD', TRUE),
-(3, 'A', 2, 'STANDARD', TRUE),
-(3, 'A', 3, 'STANDARD', TRUE),
-(3, 'A', 4, 'STANDARD', TRUE),
-(3, 'A', 5, 'STANDARD', TRUE),
-(3, 'A', 6, 'STANDARD', TRUE),
-(3, 'A', 7, 'STANDARD', TRUE),
-(3, 'A', 8, 'STANDARD', TRUE),
-(3, 'A', 9, 'STANDARD', TRUE),
-(3, 'A', 10, 'STANDARD', TRUE);
 
 
 -- =========================
@@ -168,20 +122,6 @@ CREATE TABLE showtime (
         CHECK (ends_at > starts_at)
 );
 
-INSERT INTO showtime (
-    room_id,
-    movie_id,
-    starts_at,
-    ends_at,
-    status
-) VALUES (
-    1,
-    1,
-    '2026-01-10 10:00:00',
-    '2026-01-10 12:00:00',
-    'OPEN'
-);
-
 
 
 
@@ -202,17 +142,7 @@ CREATE TABLE IF NOT EXISTS client (
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO client (nom, address, age) VALUES
-('Client 1',  'Adresse 1',  22),
-('Client 2',  'Adresse 2',  25),
-('Client 3',  'Adresse 3',  30),
-('Client 4',  'Adresse 4',  28),
-('Client 5',  'Adresse 5',  35),
-('Client 6',  'Adresse 6',  40),
-('Client 7',  'Adresse 7',  18),
-('Client 8',  'Adresse 8',  27),
-('Client 9',  'Adresse 9',  33),
-('Client 10', 'Adresse 10', 45);
+
 
 
 -- =========================
@@ -275,17 +205,7 @@ CREATE INDEX IF NOT EXISTS idx_reservation_ticket ON reservation(ticket_id);
 CREATE INDEX IF NOT EXISTS idx_reservation_status ON reservation(status);
 
 
-INSERT INTO reservation (ticket_id, client_id, status) VALUES
-(1, 1, 'PAYER'),
-(1, 2, 'PAYER'),
-(1, 3, 'NON_PAYER'),
-(1, 4, 'PAYER'),
-(1, 5, 'NON_PAYER'),
-(1, 6, 'PAYER'),
-(1, 7, 'PAYER'),
-(1, 8, 'NON_PAYER'),
-(1, 9, 'PAYER'),
-(1, 10, 'PAYER');
+
 
 
 
